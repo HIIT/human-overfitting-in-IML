8f99b741deda3de58dfc48e5cde91306671e9b2a
PedramDaee <pedram.daee@gmail.com>
2017-09-14 12:19:30 +0300

    Added more statistical tests.

--- evaluate.m
+++ evaluate.m
@@ -19,8 +19,7 @@ disp(['Number of users ', num2str(num_users), ' runs.']);
 
 figure
 Loss_1_mean = mean(Loss_1,3)';
-%This one is the data
-% plot([0:num_iterations-1],Loss_1_mean,'.-','LineWidth',2);
+%Iterations are not used in this study
 plot([1:num_iterations],Loss_1_mean,'.-','LineWidth',2);
 legend(Method_list)
 title('Loss function')
@@ -28,7 +27,7 @@ xlabel('Number of Expert Feedbacks')
 ylabel('Mean Squared Error')
 
 %statistical analysis of improvements
-GT_methods_loss_1 = Loss_1(:,1,:);
+GT_methods_loss_1 = Loss_1(:,1,:);  %Iterations are not used in this study
 %TODO: find methods that have zero std and call them gt methods first
 GT_methods_loss_1 = reshape(GT_methods_loss_1,num_methods,num_users);
 figure
@@ -49,13 +48,29 @@ title('Histogram of Loss function')
 xlabel('MSE')
 legend(Method_list)
 
-%Fin the targetter methods:
+%Find the target methods:
 method_OF_user = find(strcmp('User FB before correction', Method_list));
 method_inferred_user = find(strcmp('User FB after correction', Method_list));
-disp('is the inferred results better?')
+method_no_feedback = find(strcmp('no feedback', Method_list)); 
+
+%Test 1: feedback is better than no feedback
+disp('Is receiving feedback in general better than not receiving it?')
+[h,p,ci,stats] = ttest(GT_methods_loss_1(method_no_feedback,:),GT_methods_loss_1(method_OF_user,:)); %, 'Alpha',0.05
+if h
+    disp(['Yes, with p-value=',num2str(p), ' and CI=',num2str(ci) ]);
+else
+    disp(['No, with p-value=',num2str(p), ' and CI=',num2str(ci) ]);
+end
+
+%Test 2: user model is better than feedback
+disp('Is the inferred results better than feedback?')
 % disp( GT_methods_loss_1(method_OF_user,:) - GT_methods_loss_1(method_inferred_user,:))
-%I guess h should be one
-[h,p,ci,stats] = ttest(GT_methods_loss_1(method_OF_user,:),GT_methods_loss_1(method_inferred_user,:)) %, 'Alpha',0.05
+[h,p,ci,stats] = ttest(GT_methods_loss_1(method_OF_user,:),GT_methods_loss_1(method_inferred_user,:)); %, 'Alpha',0.05
+if h
+    disp(['Yes, with p-value=',num2str(p), ' and CI=',num2str(ci) ]);
+else
+    disp(['No, with p-value=',num2str(p), ' and CI=',num2str(ci) ]);
+end
 
 figure
 hold on
